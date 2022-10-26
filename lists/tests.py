97c87128550d7d26f0c4dc2be7784d48030dd41b
Leandro Diniz Silva <leandrodinizsilva@users.noreply.github.com>
2022-10-25 21:04:45 -0300

    Implementando o Novo Projeto (Listas e Items) - PARTE 3

--- lists/tests.py
+++ lists/tests.py
@@ -6,6 +6,9 @@ from lists.views import home_page
 from lists.models import Item
 
 class ListViewTest(TestCase):
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
 
     def test_displays_all_items(self):
         Item.objects.create(text='itemey 1')
