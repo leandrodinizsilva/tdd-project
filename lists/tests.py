63d6828682d337024ae9c5324b90c3a602ce07fc
Leandro Diniz Silva <leandrodinizsilva@users.noreply.github.com>
2022-10-25 21:25:37 -0300

    Implementando o Novo Projeto: Ajustando o Resto do Sistema

--- lists/tests.py
+++ lists/tests.py
@@ -23,8 +23,9 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'list.html')
 
     def test_displays_all_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+        my_list = List.objects.create()
+        Item.objects.create(text='itemey 1', list=my_list)
+        Item.objects.create(text='itemey 2', list=my_list)
 
         response = self.client.get('/lists/the-only-list-in-the-world/')
 
@@ -36,23 +37,6 @@ class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
-
-    def test_saving_and_retrieving_items(self):
-        first_item = Item()
-        first_item.text = 'O primeiro item'
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = 'O segundo item'
-        second_item.save()
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, 'O primeiro item')
-        self.assertEqual(second_saved_item.text, 'O segundo item')
 
 class ListAndItemModelsTest(TestCase):
 
